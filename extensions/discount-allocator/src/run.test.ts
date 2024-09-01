e27e1ee1c3be452e7ce149394ff759ac536fd948
Paulo <paulo@desktech.com>
2024-09-01 14:33:51 -0400

    testing and bug fixing

--- extensions/discount-allocator/src/run.test.ts
+++ extensions/discount-allocator/src/run.test.ts
@@ -1,12 +1,45 @@
 import {describe, it, expect} from 'vitest';
 import {run} from './run';
-import fs from "fs";
-import { RunInput } from '../generated/api';
+import { DiscountApplicationStrategy, DiscountClass, FixedAmount, RunInput } from '../generated/api';
+import { DiscountInput, first, ShopifyEntity, shopifyGid } from './utils';
+import { LineDiscount } from '../generated/api';
+
+const value = { amount: 5} as FixedAmount;
+const orderFirstDiscount: DiscountInput = {
+  id: shopifyGid(ShopifyEntity.Discount, "order-first"),
+  code: "TEST1",
+  discountClass: DiscountClass.Order,
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discountProposals: [
+    {
+      handle: "order-first-proposal",
+      value,
+      targets: [
+        {
+          cartLineId: shopifyGid(ShopifyEntity.CartLine, "order-first-line"),
+          quantity: 1
+        }
+      ]
+    }
+  ]
+
+}
+
+const orderFirstBiMonthly: RunInput = {
+  cart: {
+    plan: { value: "Bi-Monthly"},
+    lines: []
+  },
+  discounts:[
+   orderFirstDiscount
+  ]
+}
 
 describe('discounts allocator function', () => {
-  it('input.json', () => {
-    const input = JSON.parse(fs.readFileSync('input.json').toString()) as RunInput;
-    const result = run(input);
-    expect(result.lineDiscounts?.pop()?.allocations.pop()?.amount ?? 0).toBe(2.5)
+  it('should halve fixed discount for order when cart plan is Bi-Monthly and discountClass is ORDER and discountStrategy is FIRST', () => {
+    const { lineDiscounts, displayableErrors } = run(orderFirstBiMonthly);
+    const { amount } = first(first(lineDiscounts).allocations);
+    expect(displayableErrors?.length).toBe(0);
+    expect(amount).toBe(2.5);
   })
 });
--- extensions/discount-allocator/src/run.ts
+++ extensions/discount-allocator/src/run.ts
@@ -10,33 +10,7 @@ import {
   Value,
   CartLineTarget,
 } from "../generated/api.ts";
-
-enum PaymentPlan {
-  None = "",
-  Monthly = "Monthly",
-  BiMonthly = "Bi-Monthly",
-  EveryTwoWeeks = "Every Two Weeks"
-}
-
-type ArrayElement<ArrayType extends readonly unknown[] | null | undefined> = 
-  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
-
-type DiscountInput = ArrayElement<RunInput["discounts"]>;
-type LineInput = ArrayElement<RunInput["cart"]["lines"]>;
-
-function reduceByPaymentPlan(plan: PaymentPlan, amount: number) {
-  switch(plan) {
-    case PaymentPlan.BiMonthly:
-    case PaymentPlan.EveryTwoWeeks:
-      return amount / 2;
-    default:
-      return amount;
-  }
-}
-
-function fixedAmount(value: Value) {
-  return Number((value as FixedAmount).amount);
-}
+import { DiscountInput, fixedAmount, LineInput, mergeResults, PaymentPlan, reduceByPaymentPlan, ShopifyEntity, shopifyGid, strToPaymentPlan } from "./utils.ts";
 
  function findProposalByStrategy(strategy: DiscountApplicationStrategy, proposals: DiscountProposal[]): DiscountProposal[] {
   switch(strategy) {
@@ -58,7 +32,7 @@ function fixedAmount(value: Value) {
     allocations: [
       {
         amount,
-        discountProposalId: `gid://shopify/DiscountProposal/${proposalHandle}`
+        discountProposalId: shopifyGid(ShopifyEntity.DiscountProposal, proposalHandle)
       }
     ]
   }) as LineDiscount);
@@ -66,12 +40,10 @@ function fixedAmount(value: Value) {
 
 
 
-const mergeResults = (...results: FunctionRunResult[]) => results.reduce((p, c) => ({ 
-  displayableErrors: [...(p.displayableErrors ?? []), ...(c.displayableErrors ?? [])],
-  lineDiscounts: [...(p.lineDiscounts ?? []), ...(p.lineDiscounts ?? [])]
-}), {});
+
 
 function allocateOrderDiscount(discount: DiscountInput, cartPlan: PaymentPlan): FunctionRunResult {
+  console.log({ cartPlan });
   const lineDiscounts: LineDiscount[] = []
   const displayableErrors: DisplayableError[] = []
   for (let proposal of findProposalByStrategy(discount.discountApplicationStrategy, discount.discountProposals as DiscountProposal[])) {
@@ -102,7 +74,7 @@ function allocateProductDiscount(discount: DiscountInput, lines: LineInput[]): F
     .map(({ cartLineId, quantity }) => {
       const { plan } = lines.find(({ id }) => id === cartLineId) ?? {};
       const amount = fixedAmount(proposal.value);
-      const linePlan = PaymentPlan[plan?.value ?? ""];
+      const linePlan = strToPaymentPlan(plan?.value ?? "");
       const allocation = reduceByPaymentPlan(linePlan, amount);
       return {
         cartLineId,
@@ -110,7 +82,7 @@ function allocateProductDiscount(discount: DiscountInput, lines: LineInput[]): F
         allocations: [
           {
             amount: allocation,
-            discountProposalId: `gid://shopify/DiscountProposal/${proposal.handle}`
+            discountProposalId: shopifyGid(ShopifyEntity.DiscountProposal, proposal.handle)
           }
         ]
       } as LineDiscount
@@ -131,18 +103,18 @@ function allocateProductDiscount(discount: DiscountInput, lines: LineInput[]): F
 
 export function run(input: RunInput): FunctionRunResult {
   const { plan, lines } = input.cart;
-  const result = input.discounts?.map(d => {
-    switch(d.discountClass) {
-      case DiscountClass.Order: 
-      return allocateOrderDiscount(d, PaymentPlan[plan?.value ?? ""])
-      case DiscountClass.Product:
-        return allocateProductDiscount(d, lines);
-    }
-  }) ?? [];
-  return mergeResults(...result)
+  try {
+    const result = input.discounts?.map(d => {
+      switch(d.discountClass) {
+        case DiscountClass.Order: 
+        return allocateOrderDiscount(d, strToPaymentPlan(plan?.value ?? ""))
+        case DiscountClass.Product:
+          return allocateProductDiscount(d, lines);
+      }
+    }) ?? [];
+    return mergeResults(result);
+  } catch(e) {
+    console.error(e.message, e);
+    return { displayableErrors: [{ discountId: "", reason: e.message }]};
+  }
 }
-
-
-
-
-
